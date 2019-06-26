34160506b4e942e617107d2eaf68ba413833f713
Tylian <immatyger@gmail.com>
2019-06-26 03:35:43 -0400

    Optimize Webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require( 'webpack');
 const HtmlWebpackPlugin = require( 'html-webpack-plugin');
+const path = require('path');
 
 const {
   createConfig,
@@ -21,7 +22,9 @@ const {
   entryPoint,
   env,
   setOutput,
-  sourceMaps
+  sourceMaps,
+  optimization,
+  setEnv
 } = require('webpack-blocks');
 
 const autoprefixer = require('autoprefixer');
@@ -30,14 +33,40 @@ const cssnano = require('cssnano');
 module.exports = createConfig([
   setMode(process.env.NODE_ENV || 'development'),
   entryPoint('./src/index.ts'),
-  setOutput('./dist/bundle.js'),
+  setOutput({
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    jsonpScriptType: 'module'
+  }),
+  optimization({
+    splitChunks: {
+      cacheGroups: {
+        data: {
+          chunks: 'initial',
+          name: 'data',
+          test: /data[\\/].*?\.json$/,
+          enforce: true
+        },
+        vendor: {
+          chunks: 'initial',
+          name: 'vendor',
+          test: /node_modules/,
+          enforce: true
+        },
+      }
+    },
+    runtimeChunk: 'single'
+  }),
   addPlugins([
     new HtmlWebpackPlugin({
       inject: true,
       template: 'index.html'
     })
   ]),
-
+  setEnv({
+    NODE_ENV: process.env.NODE_ENV
+  }),
   match(['*.ts', '*.tsx', '!*node_modules*'], [
     typescript()
   ]),
